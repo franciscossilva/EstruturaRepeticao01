7d6a1391d8fd932f24b7b41d7205acca66c7e79b
franciscossilva <francisco.dossantos@zup.com.br>
2023-05-30 06:34:27 -0300

    Initial commit

---
+++ src/Whilefor.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i=1; i<=100;i++){
+            //primeira vez com for
+            System.out.println(i);
+
+            int j= 0;
+            while (j < 1){
+                //segunda vez com While
+                System.out.println(i);
+                j++;
+            }
+        }
+
+    }
+}
